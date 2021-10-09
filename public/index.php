326ee9037c830a7c716994fcef127a3155d1a839
Matthew R <creations@pek.entropypoint.net>
2021-10-09 01:22:07 +0100

    Switching to Laravel Framework to recontextualise work.

--- public/index.php
+++ public/index.php
@@ -1,40 +1,55 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-    <title>OpenPresentiment</title>
-    <meta name="description" content="Software to allow anyone to replicate Dean Radin's presentiment experiments.">
-    <meta name="author" content="Matthew Riddle, Consciousness Matters">
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-    <!-- Copy out to static to enhance privacy - BEGIN -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap" rel="stylesheet">
-    <!-- Copy out to static to enhance privacy - END -->
+define('LARAVEL_START', microtime(true));
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-</head>
-<body>
-    <div id="setup">
-        <h1>Open<em>Presentiment</em> <span class="version">0.3 α1</span></h1>
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-        <div class="connect">
-            <button id="connect">Connect to Arduino</button>
-        </div>
-        <div class="options hidden">
-            <button id="participate">Participate</button>
-            <button id="clearData">Clear Data</button>
-            <button id="exportData">Export Data</button>
-        </div>
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
 
-        <canvas id="graph"></canvas>
-        <div class="axis-x-label hidden">Time in seconds (s)</div>
-        <div class="axis-y-label hidden">Voltage in millivolts (mV)</div>
-    </div>
-    <script src="js/serialDataHandler.js"></script>
-    <script src="js/graph.js"></script>
-    <script src="js/script.js"></script>
-</body>
-</html>
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
