61f5352aee2a0c9fc03945562af737cc446f4ec4
Matthew R <creations@pek.entropypoint.net>
2021-10-09 01:40:50 +0100

    Integrated OpenGSR.

---
+++ Arduino Code/GSR_V1/GSR_V1.ino
@@ -0,0 +1,59 @@
+#include <Adafruit_ADS1X15.h>
+
+Adafruit_ADS1115 ads;
+unsigned long sampleWindowTime = 40000; // 50000 =  20 FPS / 40000 = 25 FPS / 33333 = 30 FPS
+unsigned long samples = 0;
+unsigned long lastSamples = 0;
+unsigned long lastSerialOutputTime = 0;
+float sampleAccumulatorValue = 0;
+int sampleAccumulatorSetSize = 0;
+
+void setup(void)
+{
+  Serial.begin(9600);
+  ads.setGain(GAIN_TWO); // Max gain that wont overscale for pads touching.
+  ads.setDataRate(RATE_ADS1115_860SPS);
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+}
+
+void loop(void)
+{
+  unsigned long runtime;
+  int16_t adc0, adc1, adc2, adc3, diff01;
+  float adcAverage, adcAverageVolts, adcSuperSampledAverage, adcSuperSampledAverageVolts, adc0Volt;
+  unsigned long nextSerialOutputTime, samplesThisWindow;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+  adcAverage = ((float) adc0 + (float) adc1 + (float) adc2 + (float) adc3) / 4;
+
+  sampleAccumulatorValue = sampleAccumulatorValue + adcAverage;
+  sampleAccumulatorSetSize++;
+
+  nextSerialOutputTime = lastSerialOutputTime + sampleWindowTime;
+  runtime = micros();
+
+  if (nextSerialOutputTime <= runtime) {
+    samplesThisWindow = samples - lastSamples;
+    adcSuperSampledAverage = (float) sampleAccumulatorValue / (float) sampleAccumulatorSetSize;
+    adcSuperSampledAverageVolts = adcSuperSampledAverage * 0.0625;
+    adc0Volt = (float) adc0 * 0.0625;
+    
+    Serial.print(millis()); Serial.print(","); Serial.println(adcSuperSampledAverageVolts);
+//    Serial.print(millis()); Serial.print(","); Serial.print(samplesThisWindow); Serial.print(","); Serial.println(adcSuperSampledAverageVolts);
+//    Serial.print(adc0); Serial.print(","); Serial.print(adcAverage); Serial.print(","); Serial.println(adcSuperSampledAverage);
+    lastSerialOutputTime = runtime;
+    lastSamples = samples;
+
+    sampleAccumulatorValue = 0;
+    sampleAccumulatorSetSize = 0;
+    
+  }
+
+  samples++;
+}
---
+++ Arduino Code/GSR_V1b/GSR_V1b.ino
@@ -0,0 +1,55 @@
+unsigned long sampleWindowTime = 40000; // 50000 =  20 FPS / 40000 = 25 FPS / 33333 = 30 FPS
+unsigned long samples = 0;
+unsigned long lastSamples = 0;
+unsigned long lastSerialOutputTime = 0;
+float sampleAccumulatorValue = 0;
+int sampleAccumulatorSetSize = 0;
+
+void setup(void)
+{
+  Serial.begin(9600);
+}
+
+void loop(void)
+{
+  unsigned long runtime;
+  int a0, a1, a2, a3, a4, a5;
+  float aSuperSampledAverage, aSuperSampledAverageVolts, aAverage, aVolt, a0Volt;
+  unsigned long nextSerialOutputTime, samplesThisWindow;
+
+  a0 = (int) analogRead(A0);
+  a1 = (int) analogRead(A1);
+  a2 = (int) analogRead(A2);
+  a3 = (int) analogRead(A3);
+  a4 = (int) analogRead(A4);
+  a5 = (int) analogRead(A5);
+  
+  aAverage = ((float) a0 + (float) a1 + (float) a2 + (float) a3 + (float) a4 + (float) a5) / 6;
+
+  sampleAccumulatorValue = sampleAccumulatorValue + aAverage;
+  sampleAccumulatorSetSize++;
+
+  nextSerialOutputTime = lastSerialOutputTime + sampleWindowTime;
+  runtime = micros();
+
+  if (nextSerialOutputTime <= runtime) {
+    samplesThisWindow = samples - lastSamples;
+    aSuperSampledAverage = (float) sampleAccumulatorValue / (float) sampleAccumulatorSetSize;
+    aSuperSampledAverageVolts = aSuperSampledAverage * 4.888;
+    a0Volt = (float) a0 * 4.888;
+
+    // Raw sensor output
+//    Serial.print(a0); Serial.print(","); Serial.print(a1); Serial.print(","); Serial.print(a2); Serial.print(","); Serial.print(a3); Serial.print(","); Serial.print(a4); Serial.print(","); Serial.println(a5);
+    // Raw volts vs supersampled
+//    Serial.print(a0Volt); Serial.print(","); Serial.println(aSuperSampledAverageVolts);
+    // Standard output
+    Serial.print(millis()); Serial.print(","); Serial.println(aSuperSampledAverageVolts);
+    lastSerialOutputTime = runtime;
+    lastSamples = samples;
+
+    sampleAccumulatorValue = 0;
+    sampleAccumulatorSetSize = 0;
+  }
+
+  samples++;
+}
