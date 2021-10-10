6838ed5fdcf7af8ce5b82eca407ddf39ba0abe03
Matthew R <creations@pek.entropypoint.net>
2021-10-10 14:59:51 +0100

    Installed laravel breeze.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/opengsr', function () {
-    return view('opengsr');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
