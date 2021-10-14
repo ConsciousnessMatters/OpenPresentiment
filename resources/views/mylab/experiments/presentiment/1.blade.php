8cd6783c6db9a5fd61866994332bdc4030edf2a9
Matthew R <creations@pek.entropypoint.net>
2021-10-15 00:07:16 +0100

    Added some design flare.

--- resources/views/mylab/experiments/presentiment/1.blade.php
+++ resources/views/mylab/experiments/presentiment/1.blade.php
@@ -4,10 +4,10 @@
             <h1>Experiment 1, Dean Radin, PRNG</h1>
         </section>
         <section>
-            <h2>Part 1, Subject Selection</h2>
-            <h2>Part 2, Subject Agreement</h2>
-            <h2>Part 3, Trials</h2>
-            <h2>Part 4, Thanks & Further Information</h2>
+            <span class="part current">Part 1, Subject Selection</span>
+            <span class="part">Part 2, Subject Agreement</span>
+            <span class="part">Part 3, Trials</span>
+            <span class="part">Part 4, Thanks & Further Information</span>
         </section>
     </main>
 </x-app-layout>
