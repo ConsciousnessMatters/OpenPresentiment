7c4ea733d81599258002b1f81c95f744109759bb
Matthew R <creations@pek.entropypoint.net>
2021-10-14 23:45:27 +0100

    Tinkering with how I'm going to do Experiment 1.

--- resources/views/mylab/experiments/presentiment/1.blade.php
+++ resources/views/mylab/experiments/presentiment/1.blade.php
@@ -1,6 +1,13 @@
 <x-app-layout>
     <main class="mylab experiment presentiment xp1">
-        <h1>Experiment 1, Dean Radin, PRNG</h1>
-        <h2>Part 1, Subject Selection</h2>
+        <section>
+            <h1>Experiment 1, Dean Radin, PRNG</h1>
+        </section>
+        <section>
+            <h2>Part 1, Subject Selection</h2>
+            <h2>Part 2, Subject Agreement</h2>
+            <h2>Part 3, Trials</h2>
+            <h2>Part 4, Thanks & Further Information</h2>
+        </section>
     </main>
 </x-app-layout>
