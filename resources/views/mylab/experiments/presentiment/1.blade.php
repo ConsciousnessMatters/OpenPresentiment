ba0c79aaaa04c016633033401de5fc45b690d2b4
Matthew R <creations@pek.entropypoint.net>
2021-10-16 17:23:15 +0100

    Sketched out trials.

--- resources/views/mylab/experiments/presentiment/1.blade.php
+++ resources/views/mylab/experiments/presentiment/1.blade.php
@@ -18,6 +18,32 @@
         </section>
         <section class="part part-4 hidden">
             <h1>Part 4</h1>
+            <p>We'll need confirmation that the GSR is online and working before trials begin.</p>
+
+            <button type="button">Start</button>
+
+            <div class="trials hidden">
+                <div>
+                    Click the button or press space to continue when you're ready.
+                    Progress: 1 of 16
+                </div>
+                <div>
+                    T -7
+                </div>
+                <div>
+                    Show Picture
+                </div>
+                <div>
+                    T +10
+                </div>
+                <div>
+                    T +10
+                </div>
+
+                <div>
+                    End only: Trials Complete
+                </div>
+            </div>
         </section>
         <section class="part part-5 hidden">
             <h1>Part 5</h1>
