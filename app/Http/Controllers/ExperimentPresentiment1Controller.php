f5943c08015dadcac642b3f28504a8e1f36973ef
Matthew R <creations@pek.entropypoint.net>
2021-10-19 20:10:51 +0100

    Data is now caught and stored, minimally.

--- app/Http/Controllers/ExperimentPresentiment1Controller.php
+++ app/Http/Controllers/ExperimentPresentiment1Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trial;
 use Illuminate\Http\Request;
 
 class ExperimentPresentiment1Controller extends ExperimentPresentimentController
@@ -18,9 +19,14 @@ class ExperimentPresentiment1Controller extends ExperimentPresentimentController
         $gsrData = $request->input('gsrData');
         $eventData = $request->input('eventData');
 
+        $trial = new Trial();
+        $trial->experiment_id = 1;
+        $trial->gsr_data = $gsrData;
+        $trial->event_data = $eventData;
+        $success = $trial->save();
+
         return response()->json([
-            'gsrData' => $gsrData,
-            'eventData' => $eventData,
+            'success' => $success,
         ]);
     }
 }
