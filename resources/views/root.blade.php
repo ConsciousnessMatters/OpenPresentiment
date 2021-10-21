e770611b754c09b94896d031ff4d9cabb73abf98
Matthew R <creations@pek.entropypoint.net>
2021-10-21 23:58:58 +0100

    Marking 0.6 alpha

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout bodyClass="{{ $bodyClass }}">
     <header>
         <h1>Open<em>Presentiment</em></h1>
-        <div class="version">0.4 alpha</div>
+        <div class="version">0.6 alpha</div>
     </header>
     <div class="options">
         @auth
